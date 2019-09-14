7e2156e0e33694343655b89fe668e5c097673768
fanyang <fanyang.coder@gmail.com>
2019-09-14 19:37:44 +0800

    add node main.go

---
+++ handler.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"github.com/fanyang1988/eos-light-node/p2p"
+	"github.com/fanyang1988/eos-light-node/verifier"
+	"github.com/fanyang1988/force-block-ev/log"
+	"go.uber.org/zap"
+)
+
+func startHandler() *p2p.HandlerToChannel {
+	channel := make(chan p2p.MsgToChan, 1024)
+	log.Logger().Info("start handler")
+	go func(ch chan p2p.MsgToChan) {
+		verifier := verifier.NewBlockVerifier(log.Logger())
+		for {
+			msg, ok := <-ch
+			if !ok {
+				log.Logger().Error("handler chan close")
+				return
+			}
+
+			if msg.CloseReason != 0 {
+				log.Logger().Error("handler chan close", zap.Uint8("reason", msg.CloseReason-1))
+				return
+			}
+
+			log.Logger().Info("handler block", zap.String("block", msg.Block.String()))
+			err := verifier.Verify(&msg.Block)
+			if err != nil {
+				log.Logger().Error("verify error", zap.Error(err))
+			}
+		}
+	}(channel)
+
+	return p2p.NewHandlerToChannel(channel)
+}
