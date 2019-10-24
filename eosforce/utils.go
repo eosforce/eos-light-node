cba9f06dc2cb759faa0787e147964b05b425cd64
fanyang <fanyang.coder@gmail.com>
2019-10-24 13:39:34 +0800

    add eosforce gensis

---
+++ eosforce/utils.go
@@ -0,0 +1,16 @@
+package eosforce
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+// LoadJSONFile load a json file to obj
+func LoadJSONFile(path string, obj interface{}) error {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(b, obj)
+}
